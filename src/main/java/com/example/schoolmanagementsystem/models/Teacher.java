6d35258ad22df7dff3544f845c4a566be1402881
deca <isuleimanibrahim@gmail.com>
2022-10-26 16:27:46 +0100

    added all Teachers field

--- src/main/java/com/example/schoolmanagementsystem/models/Teacher.java
+++ src/main/java/com/example/schoolmanagementsystem/models/Teacher.java
@@ -1,4 +1,12 @@
 package com.example.schoolmanagementsystem.models;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter @Setter @RequiredArgsConstructor @Entity
+public class Teacher extends Person{
+    String officeAddress;
 }
