b977f3eb54a7bf7304fdce768f4fd507735aa9fe
deca <isuleimanibrahim@gmail.com>
2022-10-26 16:11:35 +0100

    added field to Person class

--- src/main/java/com/example/schoolmanagementsystem/models/Person.java
+++ src/main/java/com/example/schoolmanagementsystem/models/Person.java
@@ -1,5 +1,19 @@
 package com.example.schoolmanagementsystem.models;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 abstract class Person {
+
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    String dob;
+    String address;
 
 }
