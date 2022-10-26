99ff1d252085f7cea1850151fcb9ec9b7cb33236
deca <isuleimanibrahim@gmail.com>
2022-10-26 16:53:26 +0100

    added student repository

---
+++ src/main/java/com/example/schoolmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,21 @@
+package com.example.schoolmanagementsystem.repository;
+
+import com.example.schoolmanagementsystem.enums.StudentClass;
+import com.example.schoolmanagementsystem.enums.Subject;
+import com.example.schoolmanagementsystem.enums.Terms;
+import com.example.schoolmanagementsystem.models.Students;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Students, Long> {
+
+    Optional<Students> findStudentsByStudentClass(StudentClass studentClass);
+
+    Optional<Students> findStudentsBySubject(Subject subject);
+
+    Optional<Students> findStudentsByTerms(Terms terms);
+
+}
