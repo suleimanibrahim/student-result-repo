4e79eda8ee97f507d23072d2e4e77fc881246f0e
deca <isuleimanibrahim@gmail.com>
2022-10-26 16:24:58 +0100

    added all student field

--- src/main/java/com/example/schoolmanagementsystem/models/Students.java
+++ src/main/java/com/example/schoolmanagementsystem/models/Students.java
@@ -2,6 +2,7 @@ package com.example.schoolmanagementsystem.models;
 
 import com.example.schoolmanagementsystem.enums.StudentClass;
 import com.example.schoolmanagementsystem.enums.Subject;
+import com.example.schoolmanagementsystem.enums.Terms;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -17,6 +18,7 @@ public class Students extends Person{
     String score;
     StudentClass studentClass;
     Subject subject;
+    Terms terms;
     String result;
 
 }
