d0a5f42856ab6636ce529f73fad6af4884051d23
deca <isuleimanibrahim@gmail.com>
2022-10-26 15:38:19 +0100

    add postgres configuration to application.properties

--- src/main/java/com/example/schoolmanagementsystem/SchoolManagementSystemApplication.java
+++ src/main/java/com/example/schoolmanagementsystem/SchoolManagementSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchoolManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementSystemApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/schoolmanagementsystem/SchoolManagementSystemApplicationTests.java
+++ src/test/java/com/example/schoolmanagementsystem/SchoolManagementSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SchoolManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
